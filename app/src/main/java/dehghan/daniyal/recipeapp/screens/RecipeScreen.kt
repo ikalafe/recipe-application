c3a9c2163e2a121b3307698456aa326bc03e52cd
Daniyal <daniyaldehghan2021@gmail.com>
2024-06-27 19:14:42 +0330

    add fun RecipeScreen in RecipeScreen File

--- app/src/main/java/dehghan/daniyal/recipeapp/screens/RecipeScreen.kt
+++ app/src/main/java/dehghan/daniyal/recipeapp/screens/RecipeScreen.kt
@@ -1,2 +1,9 @@
 package dehghan.daniyal.recipeapp.screens
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RecipeScreen(modifier: Modifier = Modifier) {
+
+}
